4b2492d70eb6f8a3701c180cff1fbe9106a96a19
Allen <andycc77e@gmail.com>
2017-04-13 17:45:23 +0800

    建立測試數據

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,22 @@
 |
 */
 
-$factory->define(App\User::class, function ($faker) {
+$factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
+        'avatar'=> $faker->imageUrl(256,256),
         'password' => str_random(10),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Discussion::class, function (Faker\Generator $faker) {
+    $user_ids = \App\User::lists('id')->toArray();
+    return [
+        'title' => $faker->name,
+        'body' => $faker->email,
+        'user_id' => $faker->randomElement($user_ids),
+        'last_user_id' => $faker->randomElement($user_ids),
     ];
 });
